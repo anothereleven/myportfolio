2fc7ae954d2a2831d5aa191d971682af733fe66d
Anurag Patil <anurag3103@gmail.com>
2023-12-20 08:49:23 -0800

    first commit portfolio website

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='App-nameheader'>
+          Hi! I am Anurag
+        </p>
+        <p className="App-subheader">
+          I am a Designer/Developer/Marketing Enthusiast
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get to know me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
