64698edb9c384dfb648e87f5aa79e6069dfec37b
Anurag Patil <anurag3103@gmail.com>
2023-12-31 10:55:20 -0800

    click link to mail and some additions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 function App() {
   return (
     <div class="app-container">
-      <p id="app-header">
+      <p id="pheader">
         Hi I am Anurag Patil. 
       </p>
-      <p>This is my portfolio website, currently under construction.</p>
-      <p>Contact me at anurag3103@gmail.com</p>
+      <p id="psubheadercaps">
+        Designer/Developer/Marketing Enthusiast
+      </p>
+      <p id="psubheader">This is my portfolio website, currently under construction.</p>
+      <p id="psubheader">Contact me at <a id="a" href="mailto:anurag3103@gmail.com">anurag3103@gmail.com</a></p>
     </div>
   );
 }
