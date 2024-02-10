bbaf2ca832fa54017f847ea054321b0c88f0647e
Anurag Patil <anurag3103@gmail.com>
2024-02-10 11:50:22 -0800

    build fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/NavBar';
+import WorkExperience from './pages/WorkExperience';
+import Education from './pages/Education';
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
 
 function App() {
+  let component
+  switch(window.location.pathname){
+    case"/":
+    component = App
+      break
+    case"/resume":
+    component = Resume
+      break
+    case"/projects":
+    component = Projects
+      break
+    case"/workexperience":
+    component = WorkExperience
+      break
+    case"/education":
+    component = Education
+      break
+  }
   return (
     <div class="app-container">
-      <Navbar />
+      <>
+      <Navbar /> 
+      {component}
+      </>
       <p id="pheader">
         Hi I am Anurag Patil. 
       </p>
